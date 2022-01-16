bfe15cba1d4676b92d06a550df81375f63e6c7ce
Ari Glockner <44281741+AriGlockner@users.noreply.github.com>
2022-01-16 13:52:12 -0500

    Added NumArrayList Javadoc Comments

--- DataStructuresProject1/src/NumArrayList.java
+++ DataStructuresProject1/src/NumArrayList.java
@@ -1,59 +1,135 @@
-
+/**
+ * NumArrayList is
+ * 
+ * @author ari
+ */
 
 public class NumArrayList implements NumList {
-	
+	private int capacity;
+	private int size;
+
+	/**
+	 * Initializes an empty NumArrayList of capacity 0
+	 */
+	public NumArrayList() {
+		capacity = 0;
+	}
+
+	/**
+	 * Initializes an empty NumArrayList of capacity provided in the parameter
+	 * 
+	 * @param capacity
+	 */
+	public NumArrayList(int capacity) {
+		this.capacity = capacity;
+	}
+
+	/**
+	 * Returns the number of elements in the sequence
+	 */
 	@Override
 	public int size() {
-		// TODO Auto-generated method stub
-		return 0;
+		return size;
 	}
 
+	/**
+	 * Returns the number of elements the array can currently hold
+	 */
 	@Override
 	public int capacity() {
-		// TODO Auto-generated method stub
-		return 0;
+		return capacity;
 	}
 
+	/**
+	 * Adds a new element to the end of the current list
+	 * 
+	 * @param value - the element to be added to the the list
+	 */
 	@Override
 	public void add(double value) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
+	/**
+	 * Inserts a new element before the i-th element (using 0 for the index of the
+	 * first element). Thus, the inserted element becomes the i-th element. For the
+	 * case when the list has i or fewer elements, insert the new element at the end
+	 * of the list.
+	 * 
+	 * @param i     - index for the element to be inserted
+	 * @param value - the element to be added to the list
+	 */
 	@Override
 	public void insert(int i, double value) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
+	/**
+	 * Removes the i-th element of the list (using 0 for the index of the first
+	 * element). For the case when the list has fewer elements, do nothing since the
+	 * element to be removed does not exist.
+	 * 
+	 * @param i - the index to remove the element from
+	 */
 	@Override
 	public void remove(int i) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
+	/**
+	 * @param value - the value to search for
+	 * 
+	 * @return true if if the list contains value
+	 * @return false otherwise
+	 */
 	@Override
 	public boolean contains(double value) {
 		// TODO Auto-generated method stub
 		return false;
 	}
 
+	/**
+	 * @returns the i-th element of the list (using 0 for the index of the first
+	 *          element)
+	 * @throws exception if the list has fewer than i+1 elements
+	 */
 	@Override
 	public double lookup(int i) {
 		// TODO Auto-generated method stub
 		return 0;
 	}
 
+	/**
+	 * @param otherList
+	 * 
+	 * @return true if this list equals the other list
+	 * @return false otherwise
+	 */
 	@Override
 	public boolean equals(NumList otherList) {
-		// TODO Auto-generated method stub
-		return false;
+		if (this.capacity != otherList.capacity() || this.size != otherList.size())
+			return false;
+
+		return true;
 	}
 
+	/**
+	 * Removes duplicates of any elements in the list, preserving the list order and
+	 * the position of the first of the duplicates. For example, removing the
+	 * duplicates of the list [1, 2, 3, 1, 2, 4] should result in [1, 2, 3, 4].
+	 */
 	@Override
 	public void removeDuplicates() {
 		// TODO Auto-generated method stub
-		
+
 	}
 	
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 }
