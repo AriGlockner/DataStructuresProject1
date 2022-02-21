be11d1b08be931066828e96ea9e0c0ca1f952582
Ari Glockner <44281741+AriGlockner@users.noreply.github.com>
2022-02-20 20:46:14 -0500

    Completed BST Node class

--- DataStructuresProject3/src/BSTQueue.java
+++ DataStructuresProject3/src/BSTQueue.java
@@ -1,9 +1,46 @@
-public class BSTQueue
+import java.util.*;
+
+public class BSTQueue<T extends Comparable<T>, V>
 {
+	BSTNode<T, V> front;
+	BSTNode<T, V> back;
+	ArrayList<BSTNode<T, V>> queue = new ArrayList(5);
 
-
-	static class QueueNode
+	public BSTQueue()
 	{
-		
+		front = back = null;
 	}
+
+	public void add(BSTNode<T, V> node)
+	{
+		if (queue.size() == 0 || queue.contains(node))
+		{
+			queue.add(node);
+			return;
+		}
+
+		int front = 0;
+		int back = queue.size() - 1;
+
+		while (front < back)
+		{
+			int mid = (front + back) / 2;
+			//if (queue)
+		}
+	}
+
+	/*
+	static class QueueNode<T, V>
+	{
+		T key;
+		V value;
+		QueueNode<T, V> next;
+
+		public QueueNode(T key, V value)
+		{
+			this.key = key;
+			this.value = value;
+		}
+	}
+	 */
 }
