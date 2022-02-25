0913043bb6d2c6c5a472984f9776b3cce7082243
Ari Glockner <44281741+AriGlockner@users.noreply.github.com>
2022-02-25 12:22:12 -0500

    Added rotate Right-Right and Left-Left to AVL Tree

--- DataStructuresProject3/src/AVLTree.java
+++ DataStructuresProject3/src/AVLTree.java
@@ -3,25 +3,128 @@ import java.util.List;
 
 public class AVLTree<T extends Comparable<T>, V> implements BinarySearchTreeInterface<T, V>
 {
+	// Top level node of tree
 	private TreeNode<T, V> root;
 
+	/**
+	 * Instantiates a new empty AVL Tree
+	 */
 	public AVLTree()
 	{
 		root = null;
 	}
 
+	/**
+	 * Inserts a node containing key in the AVL Tree
+	 */
 	@Override
 	public void insert(T key, V value)
 	{
-		TreeNode<T, V> newNode = new TreeNode<T, V>(key, value);
+		// Create Node to add
+		TreeNode<T, V> newNode = new TreeNode<>(key, value);
 
+		// if tree is empty, add Node to tree and to list
 		if (root == null)
 		{
 			root = newNode;
 			return;
 		}
+
+		// Add Node into Tree the same way as adding it to the BST
+		TreeNode<T, V> ptr = root;
+
+		// Return statement within while loop
+		while (true)
+		{
+			// Return if value to add is the same as the node's value
+			if (ptr.value == value)
+				return;
+
+			// If keys are the same, replace the old value with this value
+			if (ptr.key.compareTo(newNode.key) == 0)
+			{
+				ptr.value = value;
+				return;
+			} else if (ptr.key.compareTo(newNode.key) > 0)
+			{
+				if (ptr.left == null)
+				{
+					ptr.left = newNode;
+					break;
+				}
+				ptr = ptr.left;
+			} else
+			{
+				if (ptr.right == null)
+				{
+					ptr.right = newNode;
+					break;
+				}
+				ptr = ptr.right;
+			}
+		}
+
+		// Rearrange Nodes as needed
+		/*
+		Rotations Needed:
+		Right-Right
+		Left-Left
+		Right-Left
+		Left-Right
+		 */
+
 	}
 
+	/**
+	 * Anti-clockwise rotation of nodes
+	 *
+	 * @param P - Parent node of node to rotate
+	 */
+	private void rotateRightRight(TreeNode<T, V> P)
+	{
+		// Return if rotation is not possible
+		if (P.right == null || P.right.right == null)
+			return;
+		TreeNode<T, V> A = P.right;
+		TreeNode<T, V> B = A.right;
+		A.right = null;
+		B.left = A;
+		P.right = B;
+	}
+
+	/**
+	 * Clockwise rotation of nodes
+	 *
+	 * @param P - Parent node of node to rotate
+	 */
+	private void rotateLeftLeft(TreeNode<T, V> P)
+	{
+		// Return if rotation is not possible
+		if (P.left == null || P.left.left == null)
+			return;
+		TreeNode<T, V> A = P.left;
+		TreeNode<T, V> B = A.left;
+		A.left = null;
+		B.right = A;
+		P.left = B;
+	}
+
+	private void rotateRightLeft(TreeNode<T, V> P)
+	{
+
+	}
+
+	private void rotateLeftRight(TreeNode<T, V> P)
+	{
+
+	}
+
+
+	/**
+	 * Searches for a node with a specific key in the BST
+	 *
+	 * @return a node with a specific key in the BST
+	 */
 	@Override
 	public V search(T key)
 	{
@@ -40,22 +143,24 @@ public class AVLTree<T extends Comparable<T>, V> implements BinarySearchTreeInte
 		return null;
 	}
 
+	/**
+	 * Deletes a node containing key from the AVL Tree if it exists
+	 */
 	@Override
 	public void delete(T key)
 	{
 
 	}
 
-	@Override
-	public V kthSmallest(int k)
-	{
-		return null;
-	}
-
+	/**
+	 * @return a list of values in inorder traversal of the AVL Tree implemented using
+	 * recursion
+	 */
 	@Override
 	public List<V> inorderRec()
 	{
-		return null;
+		if (root == null) return null;
+		return root.inorderRec();
 	}
 
 	/**
@@ -68,11 +173,21 @@ public class AVLTree<T extends Comparable<T>, V> implements BinarySearchTreeInte
 	 */
 	static class TreeNode<T extends Comparable<T>, V> implements Comparable<T>
 	{
+		// Key to be sorted by
 		private final T key;
+		// Element Node Contains
 		private V value;
+		// Left Child Node
 		private TreeNode<T, V> left;
+		// Right Child Node
 		private TreeNode<T, V> right;
 
+		/**
+		 * Instantiates a new TreeNode containing a key and a value
+		 *
+		 * @param key   - what TreeNode is sorted by
+		 * @param value - what TreeNode holds
+		 */
 		public TreeNode(T key, V value)
 		{
 			this.key = key;
@@ -80,6 +195,9 @@ public class AVLTree<T extends Comparable<T>, V> implements BinarySearchTreeInte
 			left = right = null;
 		}
 
+		/**
+		 * @return a list containing this element and all child elements in a sorted order
+		 */
 		public List<V> inorderRec()
 		{
 			List<V> list = new ArrayList<>();
@@ -95,12 +213,19 @@ public class AVLTree<T extends Comparable<T>, V> implements BinarySearchTreeInte
 			return list;
 		}
 
+		/**
+		 * @param o - other key
+		 * @return -1, 0, 1
+		 */
 		@Override
 		public int compareTo(T o)
 		{
 			return (key.compareTo(o));
 		}
 
+		/**
+		 * @return each element in inorderRec seperated by a space
+		 */
 		@Override
 		public String toString()
 		{
@@ -115,7 +240,8 @@ public class AVLTree<T extends Comparable<T>, V> implements BinarySearchTreeInte
 	}
 
 	@Override
-	public String toString() {
+	public String toString()
+	{
 		if (root == null) return "";
 		return root.toString();
 	}
@@ -127,6 +253,19 @@ public class AVLTree<T extends Comparable<T>, V> implements BinarySearchTreeInte
 	 */
 	public static void main(String[] args)
 	{
+		AVLTree<Integer, Integer> foo = new AVLTree<>();
+		foo.insert(0, 0);
+		foo.insert(1, 1);
+		foo.insert(2, 2);
+		foo.insert(-1, -1);
+		foo.insert(-2, -2);
+		System.out.println(foo);
+		foo.rotateRightRight(foo.root);
+		System.out.println(foo);
+		foo.rotateLeftLeft(foo.root);
+		System.out.println(foo);
+
+		/*
 		AVLTree<Integer, Character> tree = new AVLTree<>();
 		// Insert: 2, 1, 4, 5, 9, 3, 6, 7, 10, 12, 11
 		tree.insert(2, 'B');
@@ -171,5 +310,6 @@ public class AVLTree<T extends Comparable<T>, V> implements BinarySearchTreeInte
 		for (int i = 0; i < 5; i++)
 			tree3.insert(strings3[i], strings3[i]);
 		System.out.println("Expected:\t3.14159 ;socje JDxjnadFjk abcdefg ycienso\nActual:\t\t" + tree3);
+		*/
 	}
 }
