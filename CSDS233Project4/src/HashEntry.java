ebdd52ac087b24dfd228ccfd425b14d6cd7eab75
Ari Glockner <44281741+AriGlockner@users.noreply.github.com>
2022-03-19 21:49:21 -0400

    Implemented HashTable

--- CSDS233Project4/src/HashEntry.java
+++ CSDS233Project4/src/HashEntry.java
@@ -1,22 +1,24 @@
 /**
  * This class represents an individual Hash for the HashTable class.
- * Each Hash contains a String key that is what String is stored in
- * the Hash and a value that is used for sorting within the HashTable.
+ * Each Hash contains a String key that is used for finding this specific hash in the hash table
+ * and a value that is used for sorting within the HashTable.
  *
  * @author ari
  */
-public class HashEntry implements Comparable<Integer>
+public class HashEntry
 {
-	// String element contains
+	// The key is the index
 	private final String key;
-	// Value associated to String to be used when sorting in the HashTable class
+	// Value stored in object
 	private int value;
+	// Next hash entry in case of multiple hashes having the same key
+	private HashEntry next;
 
 	/**
 	 * Construct a new HashEntry
 	 *
-	 * @param key   what hash contains
-	 * @param value used for sorting purposes in the HastTable class
+	 * @param key   a string value that is also used to find the object in the HashTable
+	 * @param value stored in object
 	 */
 	public HashEntry(String key, int value)
 	{
@@ -41,6 +43,28 @@ public class HashEntry implements Comparable<Integer>
 	}
 
 	/**
+	 * @return next Hash Entry in linked list when multiple Hash Entries share the same location in the Hash Table
+	 */
+	public HashEntry getNext()
+	{
+		return next;
+	}
+
+	/**
+	 * Method called when two or more HashEntry's share the same position in the HashTable
+	 *
+	 * @param newHashEntry HashEntry to be added to the back of the linked list
+	 */
+	public void setNext(HashEntry newHashEntry)
+	{
+		if (next == null)
+			next = newHashEntry;
+		else
+			next.setNext(newHashEntry);
+	}
+
+
+	/**
 	 * Sets the value
 	 *
 	 * @param value used for sorting purposes within the HashTable class
@@ -50,46 +74,21 @@ public class HashEntry implements Comparable<Integer>
 		this.value = value;
 	}
 
-
 	/**
-	 * Compares this object with the specified object for order.  Returns a
-	 * negative integer, zero, or a positive integer as this object is less
-	 * than, equal to, or greater than the specified object.
-	 *
-	 * <p>The implementor must ensure {@link Integer#signum
-	 * signum}{@code (x.compareTo(y)) == -signum(y.compareTo(x))} for
-	 * all {@code x} and {@code y}.  (This implies that {@code
-	 * x.compareTo(y)} must throw an exception if and only if {@code
-	 * y.compareTo(x)} throws an exception.)
-	 *
-	 * <p>The implementor must also ensure that the relation is transitive:
-	 * {@code (x.compareTo(y) > 0 && y.compareTo(z) > 0)} implies
-	 * {@code x.compareTo(z) > 0}.
-	 *
-	 * <p>Finally, the implementor must ensure that {@code
-	 * x.compareTo(y)==0} implies that {@code signum(x.compareTo(z))
-	 * == signum(y.compareTo(z))}, for all {@code z}.
-	 *
-	 * @param o the object to be compared.
-	 * @return a negative integer, zero, or a positive integer as this object
-	 * is less than, equal to, or greater than the specified object.
-	 * @throws NullPointerException if the specified object is null
-	 * @throws ClassCastException   if the specified object's type prevents it
-	 *                              from being compared to this object.
-	 * @apiNote It is strongly recommended, but <i>not</i> strictly required that
-	 * {@code (x.compareTo(y)==0) == (x.equals(y))}.  Generally speaking, any
-	 * class that implements the {@code Comparable} interface and violates
-	 * this condition should clearly indicate this fact.  The recommended
-	 * language is "Note: this class has a natural ordering that is
-	 * inconsistent with equals."
+	 * @return "[key, value] " + any other objects in the LinkedList
 	 */
 	@Override
-	public int compareTo(Integer o)
+	public String toString()
 	{
-		if (value > o)
-			return 1;
-		if (value < o)
-			return -1;
-		return 0;
+		StringBuilder sb = new StringBuilder();
+		sb.append("[");
+		sb.append(key);
+		sb.append(", ");
+		sb.append(value);
+		sb.append("] ");
+		if (next != null)
+			sb.append(next);
+
+		return sb.toString();
 	}
 }
--- CSDS233Project4/src/HashTable.java
+++ CSDS233Project4/src/HashTable.java
@@ -1,77 +1,158 @@
 import java.lang.*;
-import java.util.ArrayList;
+import java.util.*;
 
 public class HashTable
 {
-	private ArrayList<HashEntry> table;
+	// HashTable containing all HashEntries
+	private HashEntry[] table;
 
 	/**
-	 * Initializes HashTable
+	 * Initializes HashTable to a predetermined capacity
 	 */
 	public HashTable()
 	{
-		table = new ArrayList<>();
+		table = new HashEntry[8];
 	}
 
 	/**
-	 * Initializes HashTable to a set capacity
+	 * Initializes HashTable to a specific capacity
 	 *
 	 * @param capacity - size of initial table
 	 */
 	public HashTable(int capacity)
 	{
-		table = new ArrayList<>(capacity);
+		table = new HashEntry[capacity];
 	}
 
 	/**
 	 * Adds value to
 	 *
-	 * @param key   what hash contains
-	 * @param value used for sorting purposes in the HastTable class
+	 * @param key   mapping to index in HashTable
+	 * @param value stored in HashEntry
 	 */
 	void put(String key, int value)
 	{
-		put(key, value, Math.abs(key.hashCode()));
+		int position = Math.abs(key.hashCode() % table.length);
+
+		HashEntry newHash = new HashEntry(key, value);
+
+		// Add to HashTable
+		if (table[position] == null)
+			table[position] = new HashEntry(key, value);
+			// Add to back of HashEntry Linked List
+		else
+			table[position].setNext(newHash);
 	}
 
 	/**
 	 * @param key      what hash contains
 	 * @param value    used for sorting purposes in the HastTable class
-	 * @param hashCode
+	 * @param hashCode adds new HashEntry at position determined by hashCode in parameter
 	 */
-	void put(String key, int value, int hashCode)
+	public void put(String key, int value, int hashCode)
 	{
-		if (table.size() == 0)
-		{
-			table.add(new HashEntry(key, value));
-			return;
-		}
+		int position = Math.abs(hashCode % table.length);
 
-		HashEntry hash = new HashEntry(key, value);
+		HashEntry newHash = new HashEntry(key, value);
 
-		// Binary insert
-
+		// Add hash normally
+		if (table[position] == null)
+			table[position] = new HashEntry(key, value);
+			// Add hash entry to back of linked list at that index
+		else
+			table[position].setNext(newHash);
 	}
 
+	/**
+	 * Updates the value associated with key in the hash table. If key does not exist, it will be added to the table
+	 *
+	 * @param key   what to search for
+	 * @param value the value to be updated
+	 */
 	public void update(String key, int value)
 	{
+		int position = Math.abs(key.hashCode() % table.length);
 
+		if (table[position] == null)
+			put(key, value);
+		else
+			table[position].setValue(value);
 	}
 
-
-	int get(String key)
+	/**
+	 * Returns the current value associated with the parameter key
+	 *
+	 * @param key what to search the HashTable for
+	 * @return the value associated with key if key exists and -1 if it does not
+	 */
+	public int get(String key)
 	{
-		return 0;
+		HashEntry hash = table[key.hashCode() % table.length];
+
+		// if key does not exist in table, return -1
+		if (hash == null)
+			return -1;
+		// Otherwise, return the hash's value
+		return hash.getValue();
 	}
 
-	int get(String key, int hashCode)
+	/**
+	 * Should have the same function as the other get method, but uses the provided hash code. This is to allow direct
+	 * testing of collisions.
+	 *
+	 * @param key      what to search the list of HashEntries with the same hashcode
+	 * @param hashCode what to search the HashTable for
+	 * @return the value associated with key if key exists and -1 if it does not
+	 */
+	public int get(String key, int hashCode)
 	{
-		return 0;
+		// Search every HashEntry with the key of hashCode for a HashEntry with a specific key
+		HashEntry ptr = table[Math.abs(hashCode % table.length)];
+
+		while (ptr != null)
+		{
+			if (ptr.getKey().equals(key))
+				return ptr.getValue();
+			ptr = ptr.getNext();
+		}
+
+		// Otherwise, return -1
+		return -1;
 	}
 
-	private HashEntry search()
+	/**
+	 * Doubles the size of the table and fills in the values into the new table
+	 */
+	private void increaseTableSize()
 	{
-		return null;
+		table = Arrays.copyOf(table, table.length * 2);
 	}
 
+	/**
+	 * @return every HashEntry in the HashTable
+	 */
+	@Override
+	public String toString()
+	{
+		StringBuilder sb = new StringBuilder();
+
+		for (HashEntry h : table)
+			sb.append(h).append(" ");
+
+		return sb.toString();
+	}
+
+	public static void main(String[] args)
+	{
+		HashTable t = new HashTable();
+
+		t.put("foobar", 0);
+		t.put("foobar", 0);
+		t.put("foobar", 1);
+		t.put("foo", 0);
+		t.put("foo", 1);
+		t.put("fdsjhgfjxy3yufuyc4", 983265);
+
+		System.out.println(t);
+	}
 }
