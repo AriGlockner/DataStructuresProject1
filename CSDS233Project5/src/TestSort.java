9e6b76a56f0d231b1462d0087466ba8cee2a415f
Ari Glockner <44281741+AriGlockner@users.noreply.github.com>
2022-04-05 22:20:13 -0400

    Added the function of evaluating runtimes in the TestSort class

--- CSDS233Project5/src/TestSort.java
+++ CSDS233Project5/src/TestSort.java
@@ -7,21 +7,43 @@ import java.util.Arrays;
  */
 public class TestSort
 {
+	// used for determining change in time
+	private long lastNanoTime;
+	// index for indexing for insertion into nanoTimes
+	private int nIndex = 0;
+	// Sorts are: API, insertion, quick, merge
+	private final long[][] nanoTimes = new long[9][4];
+
 	/**
 	 * Tests the sort class. Calls generateRandomArray method with n = 10, 20, 50, 100, 200, 500, 1000, 2000, and 5000
 	 */
 	@Test
 	public void testSort()
 	{
-		generateRandomArray(10);
-		generateRandomArray(20);
-		generateRandomArray(50);
-		generateRandomArray(100);
-		generateRandomArray(200);
-		generateRandomArray(500);
-		generateRandomArray(1000);
-		generateRandomArray(2000);
-		generateRandomArray(5000);
+		int[] size = new int[] { 10, 20, 50, 100, 200, 500, 1000, 2000, 5000 };
+		lastNanoTime = System.nanoTime();
+
+		for (int k : size)
+			generateRandomArray(k);
+
+		// Benchmarking
+		System.out.println("\t\t\tAPI:\t\tInsertion:\tQuick:\t\tMerge:");
+
+		for (int i = 0; i < nanoTimes.length; i++)
+		{
+			System.out.print("n = " + size[i] + ":\t");
+			// format
+			if (size[i] < 100)
+				System.out.print("\t");
+
+			for (int j = 0; j < nanoTimes[i].length; j++)
+			{
+				System.out.print(nanoTimes[i][j] + "\t");
+				if (nanoTimes[i][j] < 10000000)
+					System.out.print("\t");
+			}
+			System.out.println();
+		}
 	}
 
 	/**
@@ -35,15 +57,29 @@ public class TestSort
 		// Generate random arrays
 		int[] unsortedArray = Sort.randomArray(n, 0, n * 2);
 		int[] sortedArray = unsortedArray.clone();
+		lastNanoTime = System.nanoTime();
 
 		// Use api sort as a default sort to compare sorts to
 		Arrays.sort(sortedArray);
+		updateNanoTime(0);
 
 		// call methods that test sorts
 		testInsertionSort(unsortedArray.clone(), sortedArray);
+		updateNanoTime(1);
 		testQuickSort(unsortedArray.clone(), sortedArray);
+		updateNanoTime(2);
 		testMergeSort(unsortedArray.clone(), sortedArray);
+		updateNanoTime(3);
+
+		nIndex++;
 	}
+
+	private void updateNanoTime(int i)
+	{
+		nanoTimes[nIndex][i] = System.nanoTime() - lastNanoTime;
+		lastNanoTime = System.nanoTime();
+	}
+
 
 	/**
 	 * Tests insertion sort algorithm. Compares already sorted array to unsorted array after calling sort
