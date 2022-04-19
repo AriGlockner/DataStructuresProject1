d458954c7e9c42eef12f7d152f745c7dcb485718
Ari Glockner <44281741+AriGlockner@users.noreply.github.com>
2022-04-18 22:09:28 -0400

    Found a mistake in BFS/DFS

--- CSDS233Project6/src/TestGraph.java
+++ CSDS233Project6/src/TestGraph.java
@@ -1,9 +1,12 @@
 import org.junit.*;
 
+import java.io.PrintStream;
 import java.util.Arrays;
 
 public class TestGraph
 {
+
+
 	@Test
 	public void testFile()
 	{
@@ -11,7 +14,9 @@ public class TestGraph
 		Assert.assertEquals("A B C D\nB A C\nC A B\nD A", graph.toString());
 		graph.printGraph();
 
-		System.out.println("\n");
+		//String s = "A B C D\nB A C\nC A B\nD A";
+
+		//Assert.assertEquals("<A> <B> C D>\nB A C\nC A B\nD A", ps); //graph.printGraph());
 		Assert.assertEquals("[B, A, C]", Arrays.toString(graph.DFS("B", "C", "alphabetical")));
 		Assert.assertEquals("[B, C]", Arrays.toString(graph.BFS("B", "C", "alphabetical")));
 		Assert.assertEquals("[]", Arrays.toString(graph.secondShortestPath("D", "C")));
@@ -22,5 +27,20 @@ public class TestGraph
 	public void testEmpty()
 	{
 		Graph graph = new Graph();
+		graph.addNodes(new String[]{"A", "B", "C", "D", "E", "F"});
+		graph.addEdges("A", new String[] {"B", "C", "D"});
+		graph.addEdge("B", "E");
+		graph.addEdge("C", "F");
+		graph.addEdge("D", "E");
+		graph.addEdge("E", "F");
+		graph.addEdges("F", new String[] {"A", "C", "E"});
+		graph.printGraph();
+		//TODO: Fix. BFS and DFS should not produce the same result
+		Assert.assertEquals("[A, B, E, F]", Arrays.toString(graph.BFS("A", "F", "alphabetical")));
+		Assert.assertEquals("[A, D, E, F]", Arrays.toString(graph.BFS("A", "F", "reverse")));
+		Assert.assertEquals("[A, B, E, F]", Arrays.toString(graph.DFS("A", "F", "alphabetical")));
+		Assert.assertEquals("[A, D, E, F]", Arrays.toString(graph.DFS("A", "F", "reverse")));
+		System.out.println(Arrays.toString(graph.DFS("A", "F", "alphabetical")));
+		System.out.println(Arrays.toString(graph.DFS("A", "F", "reverse")));
 	}
 }
