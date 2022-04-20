428e400692cbc0ed4ddab7293bcbd815c026fe00
Ari Glockner <44281741+AriGlockner@users.noreply.github.com>
2022-04-20 00:43:25 -0400

    Added image

--- CSDS233Project6/src/TestWeightedGraph.java
+++ CSDS233Project6/src/TestWeightedGraph.java
@@ -49,7 +49,7 @@ public class TestWeightedGraph
 		Assert.assertTrue(graph.addWeightedEdge("Miami", "Atlanta", 549));
 		Assert.assertTrue(graph.addWeightedEdges("Dallas",
 				new String[] {"Denver", "Houston", "Chicago"},
-				new int[] {696, 199, 813}));
+				new int[] {696, 199, 806}));
 		Assert.assertTrue(graph.addWeightedEdges("Houston",
 				new String[] {"Dallas", "Atlanta"},
 				new int[] {199, 665}));
@@ -57,8 +57,8 @@ public class TestWeightedGraph
 				new String[] {"Seattle", "Los Angeles", "Dallas", "Chicago"},
 				new int[] {1099, 878, 696, 826}));
 		Assert.assertTrue(graph.addWeightedEdges("Atlanta",
-				new String[] {"Miami", "Houston", "Washington DC"},
-				new int[] {549, 665, 585}));
+				new String[] {"Miami", "Houston", "Washington DC", "Chicago", "Cleveland"},
+				new int[] {549, 665, 585, 632, 598}));
 
 		//
 		graph.printWeightedGraph();
@@ -72,10 +72,10 @@ public class TestWeightedGraph
 				Washington DC 585 Atlanta 341 Cleveland 217 New York
 				Boston 210 New York
 				Miami 549 Atlanta
-				Dallas 696 Denver 199 Houston 813 Chicago
+				Dallas 696 Denver 199 Houston 806 Chicago
 				Houston 199 Dallas 665 Atlanta
 				Denver 1099 Seattle 878 Los Angeles 696 Dallas 826 Chicago
-				Atlanta 549 Miami 665 Houston 585 Washington DC""", graph.toString());
+				Atlanta 549 Miami 665 Houston 585 Washington DC 632 Chicago 598 Cleveland""", graph.toString());
 		System.out.println();
 
 		// Shortest Path
@@ -105,11 +105,11 @@ public class TestWeightedGraph
 		// DFS Alphabetical
 		Assert.assertEquals("[Seattle, Denver, Chicago, Atlanta, Miami]",
 				Arrays.toString(graph.DFS("Seattle", "Miami", "alphabetical")));
-		Assert.assertEquals("[Boston, New York, Cleveland, Atlanta, Houston, Dallas, Chicago, Denver, Los Angeles]",
+		Assert.assertEquals("[Boston, New York, Cleveland, Atlanta, Chicago, Dallas, Denver, Los Angeles]",
 				Arrays.toString(graph.DFS("Boston", "Los Angeles", "alphabetical")));
-		Assert.assertEquals("[San Francisco, Los Angeles, Denver, Chicago, Atlanta, Washington DC]",
+		Assert.assertEquals("[San Francisco, Los Angeles, Denver, Chicago, Atlanta, Cleveland, New York, Washington DC]",
 				Arrays.toString(graph.DFS("San Francisco", "Washington DC", "alphabetical")));
-		Assert.assertEquals("[Cleveland, Atlanta, Houston]",
+		Assert.assertEquals("[Cleveland, Atlanta, Chicago, Dallas, Houston]",
 				Arrays.toString(graph.DFS("Cleveland", "Houston", "alphabetical")));
 		Assert.assertEquals("[Denver, Chicago, Atlanta]",
 				Arrays.toString(graph.DFS("Denver", "Atlanta", "alphabetical")));
@@ -117,11 +117,11 @@ public class TestWeightedGraph
 		// DFS Reverse Alphabetical
 		Assert.assertEquals("[Seattle, San Francisco, Los Angeles, Denver, Chicago, Atlanta, Miami]",
 				Arrays.toString(graph.DFS("Seattle", "Miami", "reverse")));
-		Assert.assertEquals("[Boston, New York, Cleveland, Atlanta, Houston, Dallas, Chicago, Denver, Los Angeles]",
+		Assert.assertEquals("[Boston, New York, Cleveland, Atlanta, Chicago, Dallas, Denver, Los Angeles]",
 				Arrays.toString(graph.DFS("Boston", "Los Angeles", "reverse")));
-		Assert.assertEquals("[San Francisco, Seattle, Denver, Chicago, Atlanta, Washington DC]",
+		Assert.assertEquals("[San Francisco, Seattle, Denver, Chicago, Atlanta, Cleveland, New York, Washington DC]",
 				Arrays.toString(graph.DFS("San Francisco", "Washington DC", "reverse")));
-		Assert.assertEquals("[Cleveland, Washington DC, Atlanta, Houston]",
+		Assert.assertEquals("[Cleveland, Washington DC, Atlanta, Chicago, Dallas, Houston]",
 				Arrays.toString(graph.DFS("Cleveland", "Houston", "reverse")));
 		Assert.assertEquals("[Denver, Seattle, Denver, Chicago, Atlanta]",
 				Arrays.toString(graph.DFS("Denver", "Atlanta", "reverse")));
