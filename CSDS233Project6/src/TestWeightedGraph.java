fde6b65b427b60a12a66694210628c3aaff26aa2
Ari Glockner <44281741+AriGlockner@users.noreply.github.com>
2022-04-20 01:00:01 -0400

    Created tests for real world example

--- CSDS233Project6/src/TestWeightedGraph.java
+++ CSDS233Project6/src/TestWeightedGraph.java
@@ -14,9 +14,14 @@ public class TestWeightedGraph
 		Assert.assertEquals("A 2 B 1 D\nB 3 D 10 E\nC 4 A 5 F\nD 2 C 2 E 8 F 4 G\nE 6 G\nF\nG 1 F", graph.toString());
 	}
 
+	/**
+	 * Tests a custom real world graph. This graph has a bunch of cities in the USA and time travel time, in minutes,
+	 * to get from one city to another connected city.
+	 */
 	@Test
 	public void testRealWorldExample()
 	{
+		// Create Weighted Graph
 		WeightedGraph graph = new WeightedGraph();
 
 		// Add cities
@@ -76,31 +81,30 @@ public class TestWeightedGraph
 				Houston 199 Dallas 665 Atlanta
 				Denver 1099 Seattle 878 Los Angeles 696 Dallas 826 Chicago
 				Atlanta 549 Miami 665 Houston 585 Washington DC 632 Chicago 598 Cleveland""", graph.toString());
-		System.out.println();
 
-		// Shortest Path
+		// BFS Alphabetical
 		Assert.assertEquals("[Seattle, Denver, Chicago, Atlanta, Miami]",
-				Arrays.toString(graph.shortestPath("Seattle", "Miami")));
+				Arrays.toString(graph.BFS("Seattle", "Miami", "alphabetical")));
 		Assert.assertEquals("[Boston, New York, Cleveland, Chicago, Denver, Los Angeles]",
-				Arrays.toString(graph.shortestPath("Boston", "Los Angeles")));
-		Assert.assertEquals("[San Francisco, Los Angeles, Denver, Chicago, Cleveland, Washington DC]",
-				Arrays.toString(graph.shortestPath("San Francisco", "Washington DC")));
+				Arrays.toString(graph.BFS("Boston", "Los Angeles", "alphabetical")));
+		Assert.assertEquals("[San Francisco, Los Angeles, Denver, Chicago, Atlanta, Washington DC]",
+				Arrays.toString(graph.BFS("San Francisco", "Washington DC", "alphabetical")));
 		Assert.assertEquals("[Cleveland, Atlanta, Houston]",
-				Arrays.toString(graph.shortestPath("Cleveland", "Houston")));
+				Arrays.toString(graph.BFS("Cleveland", "Houston", "alphabetical")));
 		Assert.assertEquals("[Denver, Chicago, Atlanta]",
-				Arrays.toString(graph.shortestPath("Denver", "Atlanta")));
+				Arrays.toString(graph.BFS("Denver", "Atlanta", "alphabetical")));
 
-		// Second-Shortest Path
-		// TODO: Write method, then implement these tests:
-		/*
-		System.out.println(Arrays.toString(graph.secondShortestPath("Seattle", "Miami")));
-		System.out.println(Arrays.toString(graph.secondShortestPath("Boston", "Los Angeles")));
-		System.out.println(Arrays.toString(graph.secondShortestPath("San Francisco", "Washington DC")));
-		System.out.println(Arrays.toString(graph.secondShortestPath("Cleveland", "Houston")));
-		System.out.println(Arrays.toString(graph.secondShortestPath("Denver", "Atlanta")));
-		 */
-
-		// BFS
+		// BFS Reverse Alphabetical
+		Assert.assertEquals("[Seattle, Denver, Chicago, Atlanta, Miami]",
+				Arrays.toString(graph.BFS("Seattle", "Miami", "reverse")));
+		Assert.assertEquals("[Boston, New York, Cleveland, Chicago, Denver, Los Angeles]",
+				Arrays.toString(graph.BFS("Boston", "Los Angeles", "reverse")));
+		Assert.assertEquals("[San Francisco, Seattle, Denver, Chicago, Cleveland, Washington DC]",
+				Arrays.toString(graph.BFS("San Francisco", "Washington DC", "reverse")));
+		Assert.assertEquals("[Cleveland, Atlanta, Houston]",
+				Arrays.toString(graph.BFS("Cleveland", "Houston", "reverse")));
+		Assert.assertEquals("[Denver, Chicago, Atlanta]",
+				Arrays.toString(graph.BFS("Denver", "Atlanta", "reverse")));
 
 		// DFS Alphabetical
 		Assert.assertEquals("[Seattle, Denver, Chicago, Atlanta, Miami]",
@@ -125,5 +129,29 @@ public class TestWeightedGraph
 				Arrays.toString(graph.DFS("Cleveland", "Houston", "reverse")));
 		Assert.assertEquals("[Denver, Seattle, Denver, Chicago, Atlanta]",
 				Arrays.toString(graph.DFS("Denver", "Atlanta", "reverse")));
+
+		// Shortest Path
+		Assert.assertEquals("[Seattle, Denver, Chicago, Atlanta, Miami]",
+				Arrays.toString(graph.shortestPath("Seattle", "Miami")));
+		Assert.assertEquals("[Boston, New York, Cleveland, Chicago, Denver, Los Angeles]",
+				Arrays.toString(graph.shortestPath("Boston", "Los Angeles")));
+		Assert.assertEquals("[San Francisco, Los Angeles, Denver, Chicago, Cleveland, Washington DC]",
+				Arrays.toString(graph.shortestPath("San Francisco", "Washington DC")));
+		Assert.assertEquals("[Cleveland, Atlanta, Houston]",
+				Arrays.toString(graph.shortestPath("Cleveland", "Houston")));
+		Assert.assertEquals("[Denver, Chicago, Atlanta]",
+				Arrays.toString(graph.shortestPath("Denver", "Atlanta")));
+
+		// 2nd Shortest Path
+		Assert.assertEquals("[Seattle, Denver, Chicago, Atlanta, Cleveland, Atlanta, Miami]",
+				Arrays.toString(graph.secondShortestPath("Seattle", "Miami")));
+		Assert.assertEquals("[Boston, New York, Cleveland, Chicago, Denver, Chicago, Denver, Los Angeles]",
+				Arrays.toString(graph.secondShortestPath("Boston", "Los Angeles")));
+		Assert.assertEquals("[San Francisco, Los Angeles, Denver, Chicago, Cleveland, Atlanta, Washington DC]",
+				Arrays.toString(graph.secondShortestPath("San Francisco", "Washington DC")));
+		Assert.assertEquals("[Cleveland, Atlanta, Cleveland, Atlanta, Houston]",
+				Arrays.toString(graph.secondShortestPath("Cleveland", "Houston")));
+		Assert.assertEquals("[Denver, Chicago, Cleveland, Atlanta]",
+				Arrays.toString(graph.secondShortestPath("Denver", "Atlanta")));
 	}
 }
