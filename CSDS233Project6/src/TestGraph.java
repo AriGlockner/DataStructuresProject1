72acb4a452b19a06086aaf208043951f295b90d3
Ari Glockner <44281741+AriGlockner@users.noreply.github.com>
2022-04-21 20:28:33 -0400

    Created Graph Test Cases

--- CSDS233Project6/src/TestGraph.java
+++ CSDS233Project6/src/TestGraph.java
@@ -34,6 +34,51 @@ public class TestGraph
 		Assert.assertTrue(graph.addEdges("F", new String[] {"A", "C", "E"}));
 		Assert.assertFalse(graph.addNode("D"));
 		Assert.assertFalse(graph.addEdge("C", "F"));
+		// Print Graph
+		graph.printGraph();
+		System.out.println();
+
+		// DFS
+		Assert.assertEquals("[A, B, E, F]", Arrays.toString(graph.DFS("A", "F", "alphabetical")));
+		Assert.assertEquals("[A, D, E, F]", Arrays.toString(graph.DFS("A", "F", "reverse")));
+		Assert.assertEquals("[A, D]", Arrays.toString(graph.DFS("A", "D", "alphabetical")));
+		Assert.assertEquals("[A, D]", Arrays.toString(graph.DFS("A", "D", "reverse")));
+		Assert.assertEquals("[A, B, E, F, C]", Arrays.toString(graph.DFS("A", "C", "alphabetical")));
+		Assert.assertEquals("[A, D, E, F, C]", Arrays.toString(graph.DFS("A", "C", "reverse")));
+
+		// BFS
+		Assert.assertEquals("[A, C, F]", Arrays.toString(graph.BFS("A", "F", "alphabetical")));
+		Assert.assertEquals("[A, C, F]", Arrays.toString(graph.BFS("A", "F", "reverse")));
+		Assert.assertEquals("[A, D]", Arrays.toString(graph.BFS("A", "D", "alphabetical")));
+		Assert.assertEquals("[A, D]", Arrays.toString(graph.BFS("A", "D", "reverse")));
+		Assert.assertEquals("[A, C]", Arrays.toString(graph.BFS("A", "C", "alphabetical")));
+		Assert.assertEquals("[A, C]", Arrays.toString(graph.BFS("A", "C", "reverse")));
+
+		// 2nd shortest path
+		Assert.assertEquals("[A, B, E, F]", Arrays.toString(graph.secondShortestPath("A", "F")));
+		Assert.assertEquals("[]", Arrays.toString(graph.secondShortestPath("B", "F")));
+		Assert.assertEquals("[F, A, B, E]", Arrays.toString(graph.secondShortestPath("F", "E")));
+
+		// Add Node/Nodes -> add nodes calls add node, so no need to test separately
+		Assert.assertTrue(graph.addNodes(new String[]{"G", "H"}));
+		Assert.assertFalse(graph.addNodes(new String[]{"G", "H", "I"}));
+
+		// Add edges
+		Assert.assertTrue(graph.addEdge("E", "G"));
+		Assert.assertTrue(graph.addEdges("G", new String[]{"H", "I"}));
+		Assert.assertTrue(graph.addEdge("H", "I"));
+		Assert.assertTrue(graph.addEdges("I", new String[]{"B", "G"}));
+		Assert.assertFalse(graph.addEdge("H", "I"));
+
+		// Print graph
+		graph.printGraph();
+		System.out.println();
+
+		// Remove
+		Assert.assertTrue(graph.removeNodes(new String[]{"G", "H"}));
+		Assert.assertFalse(graph.removeNodes(new String[]{"G", "H", "I"}));
+
+		// Print graph
 		graph.printGraph();
 		System.out.println();
 
@@ -45,5 +90,9 @@ public class TestGraph
 		Assert.assertEquals("[A, C, F]", Arrays.toString(graph.BFS("A", "F", "alphabetical")));
 		Assert.assertEquals("[A, C, F]", Arrays.toString(graph.BFS("A", "F", "reverse")));
 
+		// 2nd shortest path
+		Assert.assertEquals("[A, B, E, F]", Arrays.toString(graph.secondShortestPath("A", "F")));
+		Assert.assertEquals("[]", Arrays.toString(graph.secondShortestPath("B", "F")));
+		Assert.assertEquals("[F, A, B, E]", Arrays.toString(graph.secondShortestPath("F", "E")));
 	}
 }
